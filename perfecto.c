1151e380699e4dc648669aab10c391856432d109
Jose <you@example.com>
2014-11-07 10:03:54 +0100

    numeros perfectos

---
+++ perfecto.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+#include <stdlib.h>
+int main() {
+	printf("Introduce un numero\n");
+	int num;
+	int suma=0;
+	int s=1;
+	scanf("%d",&num);
+	while(s<=num){
+		if((num%s)==0){
+			suma+=s;
+		}
+		s++;
+	}
+	if((suma-num)==num){
+		printf("EL NUMERO ES PERFECTO");
+	}
+	else printf("NO ES PERFECTO");
+
+}
